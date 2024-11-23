878a78822ba4ec5f2e4ca82a174dff25c973fb99
SpruceWareDev <69159094+SpruceWareDev@users.noreply.github.com>
2024-11-23 19:39:09 +0000

    We got a square :3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, init)
+    .run();
+}
+
+fn init(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Test square
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(100.0, 100.0))),
+            material: materials.add(Color::hsl(1., 1., 1.)),
+            transform: Transform::from_xyz(0., 0., 0.),
+            ..default()
+        },
+    ));
 }
